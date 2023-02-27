05ca0ca1a49a9e0ba428e3a818c10cada4bad7f7
Katarina272727 <katarinanazarbayeva@gmail.com>
2023-02-27 12:36:19 +0600

    RK

---
+++ midterm/blogs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/blogs', views.blogs_handler),
+    path('api/blogs', views.blog_handler),
+    path('api/blogs/<int:pk>', views.blog_handler),
+    path('api/blogs/<int:pk>', views.blog_handler),
+    path('api/blogs/<int:pk>', views.blog_handler)
+]
+
